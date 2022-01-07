59e0da0e7b19e7433e144f6488a673d5d230d1c8
abazatte <abdurrahman.azattemuer@hs-osnabrueck.de>
2022-01-07 19:04:38 +0100

    Hier Maxi Für Dich

--- app/src/main/java/com/example/golfscorecard/MainActivity.java
+++ app/src/main/java/com/example/golfscorecard/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.golfscorecard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this::sendMessage);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, ScoreCardActivity.class);
+        EditText hole = findViewById(R.id.hole);
+        EditText player = findViewById(R.id.player);
+
+        String holeString = hole.getText().toString();
+        String playerString = player.getText().toString();
+
+        Bundle extra = new Bundle();
+        extra.putString("EXTRA_HOLE", holeString);
+        extra.putString("EXTRA_PLAYER", playerString);
+        intent.putExtras(extra);
+        startActivity(intent);
     }
 }
